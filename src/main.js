6e10d84b87b892546eebb422aff8fafdbfabf918
爆発は芸術です <16912240+Riajyuu@users.noreply.github.com>
2019-05-07 14:01:32 +0800

    tab showing

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+var targetLocation = "";
+
 browser.menus.create({
     id: "hide-top-domain",
     icons: {
@@ -8,14 +10,25 @@ browser.menus.create({
     contexts: ["tab"]
 });
 
-async function tabHide(url ,excludeId) {
+async function tabHide(url ,excludeId, location) {
     let query = await browser.tabs.query({url: url, currentWindow: true}),
         hidingTab = query.map((item)=>{
         return item.id
-    });
-    let index = hidingTab.indexOf(excludeId);
+    }),
+        index = hidingTab.indexOf(excludeId);
     hidingTab.splice(index, 1);
     browser.tabs.hide(hidingTab);
+    browser.menus.remove("hide-top-domain");
+    browser.menus.create({
+        id: "show-top-domain",
+        icons: {
+            "16": "icon.svg",
+            "32": "icon.svg"
+        },
+        title: "Show tabs from " + location,
+        contexts: ["tab"]
+    });
+    targetLocation = location;
 }
 
 browser.menus.onShown.addListener(async function(info, tab) {
@@ -26,10 +39,36 @@ browser.menus.onShown.addListener(async function(info, tab) {
     browser.menus.refresh();
 });
 
+async function tabShow(targetLocation) {
+    let url = "*://" + targetLocation + "/*",
+        query = await browser.tabs.query({url: url, currentWindow: true, hidden: true}),
+    showingTab = query.map((item)=>{
+        return item.id
+    });
+    browser.tabs.show(showingTab);
+    browser.menus.remove("show-top-domain");
+    browser.menus.create({
+        id: "hide-top-domain",
+        icons: {
+            "16": "icon.svg",
+            "32": "icon.svg"
+        },
+        title: "Stack tabs from ",
+        contexts: ["tab"]
+    });
+}
+
+browser.menus.onClicked.addListener(function(info) {
+    if (info.menuItemId == "show-top-domain") {
+        tabShow(targetLocation);
+    }
+})
+
 browser.menus.onClicked.addListener(function(info, tab) {
     if (info.menuItemId == "hide-top-domain") {
         let splitting = tab.url.split("/"),
-            hiding = "*://" + splitting[2] + "/*";
-        tabHide(hiding ,tab.id);
+            location = splitting[2],
+            hiding = "*://" + location + "/*";
+        tabHide(hiding ,tab.id, location);
     }
 });
